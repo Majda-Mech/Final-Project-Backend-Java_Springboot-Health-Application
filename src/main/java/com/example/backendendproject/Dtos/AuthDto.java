3fa26d21bdb88d4440986b88d7f126ceb1968641
Mechda <mechda.zariouh@gmail.com>
2023-06-09 15:57:50 +0200

    security DTOs toevoegen

---
+++ src/main/java/com/example/backendendproject/Dtos/AuthDto.java
@@ -0,0 +1,13 @@
+package com.example.backendendproject.Dtos;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+
+public class AuthDto {
+    public String username;
+    public String password;
+}
