77ee25484210a92187366b7fc110975442f3ce8e
Mechda <mechda.zariouh@gmail.com>
2023-06-09 13:29:22 +0200

    diet-repo toegevoegd

---
+++ src/main/java/com/example/backendendproject/Repositories/DietRepository.java
@@ -0,0 +1,8 @@
+package com.example.backendendproject.Repositories;
+
+
+import com.example.endprojectsmechapplication.Models.Diet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DietRepository extends CrudRepository<Diet, Long> {
+}
