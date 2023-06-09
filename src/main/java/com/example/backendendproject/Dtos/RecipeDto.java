efba1edc1ed8b71c30f87ff7ce72f9e09ce2a993
Mechda <mechda.zariouh@gmail.com>
2023-06-09 14:37:26 +0200

    Recipe-dto toegevoegd

---
+++ src/main/java/com/example/backendendproject/Dtos/RecipeDto.java
@@ -0,0 +1,25 @@
+package com.example.backendendproject.Dtos;
+
+import com.example.backendendproject.Models.Product;
+import com.example.backendendproject.Models.Recipe;
+import com.example.endprojectsmechapplication.Models.Product;
+import com.example.endprojectsmechapplication.Models.Recipe;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+public class RecipeDto {
+
+    @NotBlank(message ="Please insert a name for the Recipe" )
+    public String name;
+    public boolean isVegan;
+    public boolean isVegetarian;
+    public List<Product> products;
+
+    public RecipeDto(Recipe recipe) {
+        this.name = recipe.getName();
+        this.isVegan = recipe.isVegan();
+        this.isVegetarian = recipe.isVegetarian();
+        this.products = recipe.getProducts();
+    }
+}
