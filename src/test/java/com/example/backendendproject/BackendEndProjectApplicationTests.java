1d45334ae05225f496fbf0f28f27984d6039b51f
Mechda <mechda.zariouh@gmail.com>
2023-06-09 11:42:23 +0200

    init

---
+++ src/test/java/com/example/backendendproject/BackendEndProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEndProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
