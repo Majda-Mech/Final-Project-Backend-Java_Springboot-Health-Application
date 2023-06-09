468a25c299ff4475bb3ba4f0c1d90d54add7d7fa
Mechda <mechda.zariouh@gmail.com>
2023-06-09 14:26:59 +0200

    Product-dto toegevoegd]

---
+++ src/main/java/com/example/backendendproject/Dtos/ProductDto.java
@@ -0,0 +1,25 @@
+package com.example.backendendproject.Dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class ProductDto {
+    @NotBlank(message = "fill in a Product Name")
+    public String name;
+
+    @NotBlank(message = "Describe your product")
+    public String description;
+    public Recipe recipe;
+
+    public ProductDto(String name, String description, Recipe recipe) {
+        this.name = name;
+        this.description = description;
+        this.recipe = recipe;
+    }
+
+    public ProductDto(Product product) {
+    }
+}
