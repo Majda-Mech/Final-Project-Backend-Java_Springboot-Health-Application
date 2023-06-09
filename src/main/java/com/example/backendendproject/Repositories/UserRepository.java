2eb42dfde8b37f2050e46d298c22a519c6ce237a
Mechda <mechda.zariouh@gmail.com>
2023-06-09 15:24:43 +0200

    User-repo toegevoegd

---
+++ src/main/java/com/example/backendendproject/Repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.backendendproject.Repositories;
+
+import com.example.backendendproject.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
