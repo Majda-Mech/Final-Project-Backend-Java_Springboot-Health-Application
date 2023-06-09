98d6f679304b10c6a4d76ee9dad8f396643e62ad
Mechda <mechda.zariouh@gmail.com>
2023-06-09 14:45:43 +0200

    Role-model toegevoegd

---
+++ src/main/java/com/example/backendendproject/Repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.backendendproject.Repositories;
+
+import com.example.backendendproject.Models.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role, String> {
+}
